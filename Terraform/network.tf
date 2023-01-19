3ba589cd4df83879cd258364e119dbd1be049ba2
UGUP <upma121gupta@gmail.com>
2023-01-19 19:27:33 +0530

    Created network as per az

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -1,6 +1,32 @@
+# Resources created as per the availabilty ozones
+# Here 2 public and 2 private subnets are created in each Availability zone.
+
+resource "aws_subnet" "AS2_private_subnet_a" {
+  vpc_id            = module.AS2-VPC.vpc_id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "AS2_private_subnet_b" {
+  vpc_id            = module.AS2-VPC.vpc_id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+resource "aws_subnet" "AS2public_subnet_a" {
+  vpc_id            = module.AS2-VPC.vpc_id
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = "us-east-1a"
+}
+resource "aws_subnet" "AS2_public_subnet_b" {
+  vpc_id            = module.AS2-VPC.vpc_id
+  cidr_block        = "10.0.102.0/24"
+  availability_zone = "us-east-1b"
+}
+
 # This is VPC module 
 # In this Vpc, public,private subnets , NAT and IGW is created 
 # The route tables are also created for both Public and private subnet
+
 
 module "AS2-VPC" {
   source = "terraform-aws-modules/vpc/aws"
