2c37e7c6bd70043ebb727afa278833caf6333831
UGUP <upma121gupta@gmail.com>
2023-01-19 19:16:54 +0530

    Created network as per az

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -6,7 +6,7 @@ module "AS2-VPC" {
 
   private_subnets = [aws_subnet.private_subnet_a.id,aws_subnet.private_subnet_b.id]
   public_subnets  = [aws_subnet.public_subnet_a.id,aws_subnet.public_subnet_b.id]
-  # azs             = ["us-east-1a"]
+  azs             = ["us-east-1a","us-east-1b"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
