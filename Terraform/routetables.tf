24b2615ba00ce28a8d26601a6db6078faa3a8f41
UGUP <upma121gupta@gmail.com>
2023-01-19 22:05:30 +0530

    Created network as per az

--- Terraform/routetables.tf
+++ Terraform/routetables.tf
@@ -1,4 +1,6 @@
-resource "aws_route_table" "route-public_az-a" {
+# Creation of a Public Route Table.
+
+resource "aws_route_table" "route-public_az-ab" {
   vpc_id = module.AS2_VPC.vpc_id  
     route {
     cidr_block = "0.0.0.0/0"
@@ -6,34 +8,20 @@ resource "aws_route_table" "route-public_az-a" {
   }
 
   tags={
-    Name="public-route-table-az-a"
+    Name="public-route-table-az-ab"
   }
 
 }
 
-resource "aws_route_table" "route-public_az-b" {
-  vpc_id = module.AS2_VPC.vpc_id  
-    route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = module.AS2_VPC.igw_id
-  }
-
-  tags={
-    Name="public-route-table-az-b"
-  }
-
-}
-
-#Asscociating public subnet1 and public subnet2 with the public route table
-
-resource "aws_route_table_association" "public-route-association-public-az-a" {
+#Asscociating public subnet in availability zone a and availability zone b 
+resource "aws_route_table_association" "public-route-association-public-az-ab" {
   subnet_id      = data.aws_subnet.AS2_public_subnet_a.id
-  route_table_id = aws_route_table.route-public_az-a.id
+  route_table_id = aws_route_table.route-public_az-ab.id
 }
 
 resource "aws_route_table_association" "public-route-association-public-az-b" {
   subnet_id      = data.aws_subnet.AS2_public_subnet_b.id
-  route_table_id = aws_route_table.route-public_az-b.id
+  route_table_id = aws_route_table.route-public_az-ab.id
 }
 
 
@@ -41,7 +29,7 @@ resource "aws_route_table_association" "public-route-association-public-az-b" {
 
 #Creating a private route table Using NAT gateway
 
-resource "aws_route_table" "route-private-az-a" {
+resource "aws_route_table" "route-private-az-ab" {
   vpc_id = module.AS2_VPC.vpc_id
    
     route {
@@ -54,28 +42,15 @@ resource "aws_route_table" "route-private-az-a" {
 
 }
 
-resource "aws_route_table" "route-private-az-b" {
-  vpc_id = module.AS2_VPC.vpc_id
-   
-    route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = module.AS2_VPC.natgw_ids[1]
-  }
-  tags={
-    Name="private-route-table-az-b"
-  }
-
-}
-
 
 #Asscociating private subnet1 and private subnet2 with the private route table
 
-resource "aws_route_table_association" "private-route-association-az-a" {
+resource "aws_route_table_association" "private-route-association-az-ab" {
   subnet_id      = data.aws_subnet.AS2_private_subnet_a.id
-  route_table_id = aws_route_table.route-private-az-a.id
+  route_table_id = aws_route_table.route-private-az-ab.id
 }
 
-resource "aws_route_table_association" "private-route-association-az-b" {
+resource "aws_route_table_association" "private-route-association-az-ab" {
   subnet_id      =  data.aws_subnet.AS2_private_subnet_b.id
-  route_table_id = aws_route_table.route-private-az-b.id
+  route_table_id = aws_route_table.route-private-az-ab.id
 }
