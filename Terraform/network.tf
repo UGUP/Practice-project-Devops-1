bd6bfa5aee01e37244c6224af98f9ee730588f6c
UGUP <upma121gupta@gmail.com>
2023-01-19 19:30:40 +0530

    Created network as per az

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "AS2_private_subnet_b" {
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
 }
-resource "aws_subnet" "AS2public_subnet_a" {
+resource "aws_subnet" "AS2_public_subnet_a" {
   vpc_id            = module.AS2-VPC.vpc_id
   cidr_block        = "10.0.101.0/24"
   availability_zone = "us-east-1a"
