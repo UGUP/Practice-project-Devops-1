5e537932edbdfc948bf76cbdac1c7c9236e800b6
UGUP <upma121gupta@gmail.com>
2023-01-19 19:25:33 +0530

    Created network as per az

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -1,9 +1,15 @@
+# This is VPC module 
+# In this Vpc, public,private subnets , NAT and IGW is created 
+# The route tables are also created for both Public and private subnet
+
 module "AS2-VPC" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "AS2-VPC"
   cidr = "10.0.0.0/16"
 
+ # Here I am importing resources from networkresource.tf to created public and private 
+ # subnets as per the availability zones
   private_subnets = [aws_subnet.private_subnet_a.id,aws_subnet.private_subnet_b.id]
   public_subnets  = [aws_subnet.public_subnet_a.id,aws_subnet.public_subnet_b.id]
   azs             = ["us-east-1a","us-east-1b"]
@@ -12,6 +18,7 @@ module "AS2-VPC" {
   enable_vpn_gateway = true
 
   tags = {
+    name= "AS2-VPC"
     Terraform = "true"
     Environment = "dev"
   }
--- Terraform/networkresource.tf
+++ Terraform/networkresource.tf
@@ -1,20 +1,23 @@
-resource "aws_subnet" "private_subnet_a" {
+# Resources created as per the availabilty ozones
+# Here 2 public and 2 private subnets are created in each Availability zone.
+
+resource "aws_subnet" "AS2_private_subnet_a" {
   vpc_id            = module.AS2-VPC.vpc_id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "private_subnet_b" {
+resource "aws_subnet" "AS2_private_subnet_b" {
   vpc_id            = module.AS2-VPC.vpc_id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
 }
-resource "aws_subnet" "public_subnet_a" {
+resource "aws_subnet" "AS2public_subnet_a" {
   vpc_id            = module.AS2-VPC.vpc_id
   cidr_block        = "10.0.101.0/24"
   availability_zone = "us-east-1a"
 }
-resource "aws_subnet" "public_subnet_b" {
+resource "aws_subnet" "AS2_public_subnet_b" {
   vpc_id            = module.AS2-VPC.vpc_id
   cidr_block        = "10.0.102.0/24"
   availability_zone = "us-east-1b"
