8450d3551674b2147ace9fd80d3c5ecc0e8af70c
UGUP <upma121gupta@gmail.com>
2023-01-26 20:37:56 +0530

    Modified the creation for single nat gateway

--- Terraform/sg2.tf
+++ Terraform/sg2.tf
@@ -1,12 +1,14 @@
 resource "null_resource" "get_ip_address" {
  provisioner "local-exec" {
  command = "touch ip_address.txt && my_ip=$(curl ifconfig.me); echo $my_ip > ip_address.txt"
-
 }
 }
 
 output "ip_address" {
   value = "${file("ip_address.txt")}"
+  depends_on = [
+    null_resource.get_ip_address
+  ]
 }
 
 module "AS2_bastion_sg" {
