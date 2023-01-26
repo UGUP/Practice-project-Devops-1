c818c1b5ea5b6af5ef3b97dce47ea0496a3f01cf
UGUP <upma121gupta@gmail.com>
2023-01-26 22:59:17 +0530

    added

--- Terraform/sg.tf
+++ Terraform/sg.tf
@@ -20,9 +20,6 @@ module "AS2_bastion_sg" {
   
 }
 
-
-
-
 module "AS2_privateinstance_sg"{
     source = "terraform-aws-modules/security-group/aws"
 
@@ -42,27 +39,18 @@ module "AS2_privateinstance_sg"{
 }
 
 
-# # module "AS2_publicweb_sg"{
-# #     source = "terraform-aws-modules/security-group/aws"
+module "AS2_publicweb_sg"{
+    source = "terraform-aws-modules/security-group/aws"
 
-# #   name        = "AS2_publicweb_sg"
-# #   description = "Security group for public web"
-# #   vpc_id      = module.AS2_VPC.vpc_id
-
-# #   ingress_cidr_blocks      = ["10.10.0.0/16"]
-# #   ingress_rules            = ["https-443-tcp"]
-# #   ingress_with_cidr_blocks = [
-# #     {
-# #       from_port   = 8080
-# #       to_port     = 8090
-# #       protocol    = "tcp"
-# #       description = "User-service ports"
-# #       cidr_blocks = "10.10.0.0/16"
-# #     },
-# #     {
-# #       rule        = "postgresql-tcp"
-# #       cidr_blocks = "0.0.0.0/0"
-# #     },
-# #   ]
-
-# # }
+  name        = "AS2_publicweb_sg"
+  description = "Security group for public web"
+  vpc_id      = module.AS2_VPC.vpc_id
+  ingress_with_cidr_blocks = [{
+  from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = "${var.my_ip}/${var.bits}"
+    }
+  ]
+  egress_rules = [ "all-all"]
+}
