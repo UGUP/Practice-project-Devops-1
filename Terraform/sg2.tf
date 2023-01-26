0bb9f65e2e994886ad74c7d9d946ff5cb525d8ab
UGUP <upma121gupta@gmail.com>
2023-01-26 19:32:31 +0530

    added

--- Terraform/sg2.tf
+++ Terraform/sg2.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "get_ip_address" {
   provisioner "local-exec" {
-    command = "my_ip=$(curl ifconfig.me); echo my_ip=$my_ip > ipaddress.txt"
+    command = "my_ip=$(curl ifconfig.me); echo $my_ip > ipaddress.txt"
     # environment = {
     #   var.my_ip= null_resource.get_ip_address.local-exec.command
     #  }
